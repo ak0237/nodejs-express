1226e830dafccc985af65d5cead1c573d912a1b2
ak0237 <zabuzamomochi237@gmail.com>
2024-01-19 14:00:08 -0300

    testando conteudo dinamico com ejs

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Katy Perry Comunista', snippet: 'bla bla bla bla bla bla'},
+        {title: 'Resident Evil 4 Dating Simulator', snippet: 'bla bla bla bla bla bla'},
+        {title: 'Bully bad', snippet: 'bla bla bla bla bla bla'},
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
 
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create'});
 })
 
 // redirects
@@ -29,5 +34,5 @@ app.get('/about-us', (req, res) =>{
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', {title: '404'})
 })
